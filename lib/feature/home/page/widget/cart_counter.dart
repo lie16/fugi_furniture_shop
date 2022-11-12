86f948c68d4e49103b5eaaf9d878e6edc826316a
Yonattan <34703013+lie16@users.noreply.github.com>
2022-11-12 22:29:28 +0700

    detail page done

--- lib/feature/home/page/widget/cart_counter.dart
+++ lib/feature/home/page/widget/cart_counter.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fugi_furniture_shop/const/my_style.dart';
 
 class CartCounter extends StatefulWidget {
   const CartCounter({super.key});
@@ -13,36 +14,47 @@ class CartCounterState extends State<CartCounter> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          buildOutlinedButton(
-              icon: Icons.remove,
-              left: 8,
-              press: () {
-                if (numOfItems > 1) {
-                  setState(() {
-                    numOfItems--;
-                  });
-                }
-              }),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4),
-            child: Text(
-              numOfItems.toString().padLeft(2, '0'),
-              style: Theme.of(context).textTheme.headline6,
-            ),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.grey,
           ),
-          buildOutlinedButton(
-              icon: Icons.add,
-              right: 8,
-              press: () {
-                setState(() {
-                  numOfItems++;
-                });
-              }),
-        ],
+          // borderRadius: BorderRadius.only(
+          borderRadius: const BorderRadius.all(
+            Radius.circular(8),
+          ),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            buildOutlinedButton(
+                icon: Icons.remove,
+                left: 8,
+                press: () {
+                  if (numOfItems > 1) {
+                    setState(() {
+                      numOfItems--;
+                    });
+                  }
+                }),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: Text(
+                numOfItems.toString().padLeft(2, '0'),
+                style: MyTextStyle().labelMedium,
+              ),
+            ),
+            buildOutlinedButton(
+                icon: Icons.add,
+                right: 8,
+                press: () {
+                  setState(() {
+                    numOfItems++;
+                  });
+                }),
+          ],
+        ),
       ),
     );
   }
@@ -56,23 +68,20 @@ Widget buildOutlinedButton({
 }) {
   return InkWell(
     onTap: press,
-    child: Padding(
-      padding: const EdgeInsets.all(4.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.grey,
-          ),
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(left!),
-            bottomLeft: Radius.circular(left),
-            topRight: Radius.circular(right!),
-            bottomRight: Radius.circular(right),
-          ),
+    child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.grey,
         ),
-        child: Icon(
-          icon,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(left!),
+          bottomLeft: Radius.circular(left),
+          topRight: Radius.circular(right!),
+          bottomRight: Radius.circular(right),
         ),
+      ),
+      child: Icon(
+        icon,
       ),
     ),
   );
